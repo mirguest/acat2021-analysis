0bce185e60a698730ac15cdd8b09f16ee138d560
lintao <lintao51@gmail.com>
2021-11-16 14:35:22 +0800

    WIP: start to develop the MixingAlg.

---
+++ DummyEventMixing/src/MixingAlg.cc
@@ -0,0 +1,32 @@
+#include "MixingAlg.hh"
+
+#include "SniperKernel/AlgBase.h"
+#include "SniperKernel/AlgFactory.h"
+#include "SniperKernel/SniperLog.h"
+#include "SniperKernel/SniperPtr.h"
+
+#include "EvtNavigator/NavBuffer.h"
+
+DECLARE_ALGORITHM(MixingAlg);
+
+MixingAlg::MixingAlg(const std::string& name)
+    : AlgBase(name) {
+
+    declProp("Rates", m_rates);
+
+}
+
+bool MixingAlg::initialize() {
+
+    return true;
+}
+
+bool MixingAlg::execute() {
+
+    return true;
+}
+
+bool MixingAlg::finalize() {
+
+    return true;
+}
---
+++ DummyEventMixing/src/MixingAlg.hh
@@ -0,0 +1,31 @@
+#ifndef MixingAlg_hh
+#define MixingAlg_hh
+
+#include <map>
+
+#include <SniperKernel/AlgBase.h>
+
+#include <EvtNavigator/NavBuffer.h>
+#include <Event/SimHeader.h>
+#include <Event/OECHeader.h>
+
+class MixingAlg: public AlgBase {
+
+public:
+    MixingAlg(const std::string& name);
+
+    bool initialize();
+    bool execute();
+    bool finalize();
+
+private:
+    // rates: key is the input name, value is the rate
+    std::map<std::string, double> m_rates;
+
+private:
+    JM::NavBuffer* m_buf;
+
+};
+
+
+#endif
